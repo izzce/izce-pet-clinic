2acd26df3b9ca32e292e9956c36ea6883ad7c290
izzce <izzetcelik@gmail.com>
2019-01-19 21:53:42 +0300

    initial commit of pet-clinic boot project

---
+++ src/test/java/org/izce/petclinic/IzcePetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package org.izce.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IzcePetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
