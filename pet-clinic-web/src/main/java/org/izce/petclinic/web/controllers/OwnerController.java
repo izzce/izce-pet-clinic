423418f268efe8d7d637a477119b7f996022615f
izzce <izzetcelik@gmail.com>
2019-02-21 02:25:07 +0300

    Added a requestmapping trick to controllers.

--- pet-clinic-web/src/main/java/org/izce/petclinic/web/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/org/izce/petclinic/web/controllers/OwnerController.java
@@ -3,10 +3,11 @@ package org.izce.petclinic.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-	@RequestMapping({ "/owners", "/owners/index", "/owners/index.html" })
+	@RequestMapping({ "", "/", "/index", "/index.html" })
 	public String listOwners() {
 		return "owners/index";
 	}
--- pet-clinic-web/src/main/java/org/izce/petclinic/web/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/izce/petclinic/web/controllers/VetController.java
@@ -3,10 +3,11 @@ package org.izce.petclinic.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
+	@RequestMapping({ "", "/", "/index", "/index.html" })
 	public String listVets() {
 		return "vets/index";
 	}
