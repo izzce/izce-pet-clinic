34e6eb9c70679c332962670de0350ddcb0c921bd
izzce <izzetcelik@gmail.com>
2019-03-01 00:17:52 +0300

    list vets at their index page. Closes #15

--- pet-clinic-web/src/main/java/org/izce/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/izce/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package org.izce.petclinic.controllers;
 
+import org.izce.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+	private final VetService vetService;
+
+	@Autowired
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@RequestMapping({ "", "/", "/index", "/index.html" })
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
