185b03c3fd9493110ea6d5f34c66ce03cafbaf58
izzce <izzetcelik@gmail.com>
2019-03-09 11:00:59 +0300

    Added Visit entity and updated Owner with Pet relationship. Closes #26

--- pet-clinic-web/src/main/java/org/izce/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/org/izce/petclinic/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@RequestMapping({ "", "/", "index", "index.htm", "index.html" })
+	@RequestMapping({ "", "/", "index", "index.html" })
 	public String index() {
 		// the name of template file, omitting extension 'html'.
 		return "index";
