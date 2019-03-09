a63bc2b09cd89ea68213f5103887c97f67c02696
izzce <izzetcelik@gmail.com>
2019-03-09 11:06:29 +0300

    Added Specialty entity and updated Vet with Specialty relationship. Closes #27

---
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/Specialty.java
@@ -0,0 +1,15 @@
+package org.izce.petclinic.model;
+
+public class Specialty extends BaseEntity {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
--- pet-clinic-data/src/main/java/org/izce/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package org.izce.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
-	
+	private Set<Specialty> specialties;
+
+	public Set<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	public void setSpecialties(Set<Specialty> specialties) {
+		this.specialties = specialties;
+	}
 }
