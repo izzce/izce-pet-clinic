2f34d41cc622981ed9b0d557d2225835c9d3cebb
izzce <izzetcelik@gmail.com>
2019-03-10 13:30:54 +0300

    Created Specialties, add to Vets on Bootstrap (startup). Closes #34

--- pet-clinic-data/src/main/java/org/izce/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/services/map/VetServiceMap.java
@@ -1,10 +1,29 @@
 package org.izce.petclinic.services.map;
 
+import org.izce.petclinic.model.Specialty;
 import org.izce.petclinic.model.Vet;
+import org.izce.petclinic.services.SpecialtyService;
 import org.izce.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
-	
+	private final SpecialtyService specialtyService;
+
+	public VetServiceMap(SpecialtyService specialtyService) {
+		this.specialtyService = specialtyService;
+	}
+
+	@Override
+	public Vet save(Vet vet) {
+		if (vet.getSpecialties().size() > 0) {
+			vet.getSpecialties().forEach(specialty -> {
+				if (specialty.getId() == null) {
+					Specialty savedSpecialty = specialtyService.save(specialty);
+					specialty.setId(savedSpecialty.getId());
+				}
+			});
+		}
+		return super.save(vet);
+	}
 }
--- pet-clinic-web/src/main/java/org/izce/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/izce/petclinic/bootstrap/DataLoader.java
@@ -5,9 +5,11 @@ import java.time.LocalDate;
 import org.izce.petclinic.model.Owner;
 import org.izce.petclinic.model.Pet;
 import org.izce.petclinic.model.PetType;
+import org.izce.petclinic.model.Specialty;
 import org.izce.petclinic.model.Vet;
 import org.izce.petclinic.services.OwnerService;
 import org.izce.petclinic.services.PetTypeService;
+import org.izce.petclinic.services.SpecialtyService;
 import org.izce.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,16 +20,28 @@ public class DataLoader implements CommandLineRunner {
 	private final OwnerService ownerService;
 	private final VetService vetService;
 	private final PetTypeService petTypeService;
+	private final SpecialtyService specialtyService;
+	private boolean loaded = false;
 	
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
+		this.specialtyService = specialtyService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		if (!loaded) {
+			loadData();
+			loaded = true;
+		}
+	}
+	
+	private void loadData() {
+		
+		// PET TYPES
 		PetType dog = new PetType();
 		dog.setName("Dog");
 		PetType savedDogPetType = petTypeService.save(dog);
@@ -35,6 +49,20 @@ public class DataLoader implements CommandLineRunner {
 		PetType cat = new PetType();
 		cat.setName("Cat");
 		PetType savedCatPetType = petTypeService.save(cat);
+		
+		// SPECIALTIES
+		Specialty radiology = new Specialty();
+		radiology.setDescription("radiology");
+		Specialty savedRadiology = specialtyService.save(radiology);
+		
+		Specialty surgery = new Specialty();
+		surgery.setDescription("surgery");
+		Specialty savedSurgery = specialtyService.save(surgery);
+		
+		Specialty dentistry = new Specialty();
+		dentistry.setDescription("dentistry");
+		Specialty savedDentistry = specialtyService.save(dentistry);
+		
 		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Ayşe");
@@ -72,11 +100,13 @@ public class DataLoader implements CommandLineRunner {
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Hasan");
 		vet1.setLastName("Bakan");
+		vet1.getSpecialties().add(savedRadiology);
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
 		vet2.setFirstName("Tarkan");
 		vet2.setLastName("Yakan");
+		vet2.getSpecialties().add(savedSurgery);
 		vetService.save(vet2);
 		
 		System.out.println("Loaded vets!");
