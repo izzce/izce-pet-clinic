19bf8225ea099ca1e8c0330a40dca01e89e9b458
izzce <izzetcelik@gmail.com>
2019-03-24 21:55:33 +0300

    Implemented JPA BaseEntity. Closes #35

--- pet-clinic-data/src/main/java/org/izce/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/BaseEntity.java
@@ -2,9 +2,22 @@ package org.izce.petclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable {
-	private Long id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	//@Version private Integer version; // optimistic lock?
+	//private Timestamp creationTime;
+	//private Timestamp updateTime;
+	
 	public Long getId() {
 		return id;
 	}
