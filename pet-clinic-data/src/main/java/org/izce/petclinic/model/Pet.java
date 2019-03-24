b132bf0821be420f761bcb32518615e625bd993c
izzce <izzetcelik@gmail.com>
2019-03-25 00:02:54 +0300

    Created Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/org/izce/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package org.izce.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +25,8 @@ public class Pet extends BaseEntity {
 	private Owner owner;
 	@Column(name = "birth_date")
 	private LocalDate birthdate;
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="pet")
+	private Set<Visit> visits = new HashSet<Visit>();
 
 	public String getName() {
 		return name;
@@ -52,6 +58,14 @@ public class Pet extends BaseEntity {
 
 	public void setBirthdate(LocalDate birthdate) {
 		this.birthdate = birthdate;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
