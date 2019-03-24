ed05f6388aa219e072c03515f53f02168ddd555b
izzce <izzetcelik@gmail.com>
2019-03-24 22:52:28 +0300

    Converted Owner to JPA Entity. Closes #36

--- pet-clinic-data/src/main/java/org/izce/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/Owner.java
@@ -3,10 +3,23 @@ package org.izce.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
+	@Column(name="address")
 	private String address;
+	@Column(name="city")
 	private String city;
+	@Column(name="telephone")
 	private String telephone;
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="owner")
 	private Set<Pet> pets = new HashSet<Pet>();
 
 	public String getAddress() {
--- pet-clinic-data/src/main/java/org/izce/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/Pet.java
@@ -2,11 +2,25 @@ package org.izce.petclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+	@Column(name = "name")
 	private String name;
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType petType;
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
-	private LocalDate birthday;
+	@Column(name = "birth_date")
+	private LocalDate birthdate;
 
 	public String getName() {
 		return name;
@@ -15,7 +29,7 @@ public class Pet extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public PetType getPetType() {
 		return petType;
 	}
@@ -32,11 +46,12 @@ public class Pet extends BaseEntity {
 		this.owner = owner;
 	}
 
-	public LocalDate getBirthday() {
-		return birthday;
+	public LocalDate getBirthdate() {
+		return birthdate;
 	}
 
-	public void setBirthday(LocalDate birthday) {
-		this.birthday = birthday;
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
 	}
+
 }
