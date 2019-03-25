4e45a49344fec8d2df23a9d12dc3223fa6d7864c
izzce <izzetcelik@gmail.com>
2019-03-25 20:41:07 +0300

    Implemented Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/org/izce/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package org.izce.petclinic.services;
+
+import org.izce.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+	
+}
---
+++ pet-clinic-data/src/main/java/org/izce/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,23 @@
+package org.izce.petclinic.services.map;
+
+import org.izce.petclinic.model.Visit;
+import org.izce.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+	
+	public VisitServiceMap() {
+		
+	}
+	
+	@Override
+	public Visit save(Visit visit) {
+		if (visit.getPet() == null || visit.getPet().getId() == null 
+				|| visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+			throw new IllegalArgumentException("Invalid visit!");
+		}
+		
+		return super.save(visit);
+	}
+}
