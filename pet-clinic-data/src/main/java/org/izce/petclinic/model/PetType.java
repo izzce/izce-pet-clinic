690ae243c5f79c1ca25407894a5f6d29ce56e2ad
izzce <izzetcelik@gmail.com>
2021-02-21 14:37:39 +0300

    Fix error on Display Owner due to birthDate typo. Closes #76

--- pet-clinic-data/src/main/java/org/izce/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/org/izce/petclinic/model/PetType.java
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -26,5 +27,9 @@ public class PetType extends BaseEntity {
 		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return name;
+	}
 	
 }
