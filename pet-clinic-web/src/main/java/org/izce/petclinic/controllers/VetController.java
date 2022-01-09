3cbf04cab9eb4b75f8e4a0b85ca16594ee083587
izzce <izzetcelik@gmail.com>
2022-01-09 20:53:20 +0300

    Implemented Vets JSON Endpoint. Closes #92

--- pet-clinic-web/src/main/java/org/izce/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/izce/petclinic/controllers/VetController.java
@@ -1,10 +1,14 @@
 package org.izce.petclinic.controllers;
 
+import java.util.Set;
+
+import org.izce.petclinic.model.Vet;
 import org.izce.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VetController {
@@ -20,4 +24,10 @@ public class VetController {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
+	
+	@RequestMapping({ "/api/vets"})
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();
+	}
+	
 }
